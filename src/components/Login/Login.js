6d1ceee1f93027b971411465fe9032be2a0b7e81
Diganta165 <mdsrdiganta@gmail.com>
2021-03-18 19:13:15 +0600

    commited

---
+++ src/components/Login/Login.js
@@ -0,0 +1,52 @@
+import React, { useContext } from 'react';
+import firebase from 'firebase/app'
+
+import "firebase/auth"
+import firebaseConfig from './firebase.config';
+import {UserContext} from '../../App'
+import { useHistory, useLocation } from 'react-router';
+
+
+const Login = () => {
+    const [loggedInUser, setLoggedInUser] = useContext(UserContext);
+    const history = useHistory();
+    const location = useLocation();
+    let { from } = location.state || { from: { pathname: "/" } };
+
+    if (!firebase.apps.length) {
+        firebase.initializeApp(firebaseConfig);
+     }
+    // firebase.initializeApp(firebaseConfig);
+    const  handleGoogleSignIn =() =>{
+        var provider = new firebase.auth.GoogleAuthProvider();
+        firebase.auth()
+  .signInWithPopup(provider)
+  .then((result) => {
+    /** @type {firebase.auth.OAuthCredential} */
+    var credential = result.credential;
+
+    const {displayName, email} = result.user; // puro user info na niye just display name ar email distructure kore neya holo result.user theke
+    const signedInUser = {name: displayName, email} //signedInUser object er moddhe name(display name ta name hisebe dekhabe) r email initialize kora holo
+    setLoggedInUser(signedInUser);
+    history.replace(from);
+    // ...
+  }).catch((error) => {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+  });
+    }
+    return (
+        <div>
+            <h1>This is Login</h1>
+            <button onClick={handleGoogleSignIn}>Google SignIn</button>
+        </div>
+    );
+};
+
+export default Login;
---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD3d9k78uF0OE6_IgX6OFemTnwONawLsKo",
+    authDomain: "burj-al-arab-9dda0.firebaseapp.com",
+    projectId: "burj-al-arab-9dda0",
+    storageBucket: "burj-al-arab-9dda0.appspot.com",
+    messagingSenderId: "269465180693",
+    appId: "1:269465180693:web:03692fab7e41ba4e1c8c1c"
+  };
+  export default firebaseConfig;
